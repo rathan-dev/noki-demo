ea535f92fc8ae5b76364870c936bfdcd0c812f22
rathan-dev <rathan.dhondi@thinkhat.ai>
2024-07-23 12:51:11 +0530

    Added Navigation

---
+++ noki-demo/Models/Player.swift
@@ -0,0 +1,8 @@
+//
+//  Player.swift
+//  noki-demo
+//
+//  Created by rathan on 23/07/24.
+//
+
+import Foundation
---
+++ noki-demo/Responsiveness.swift
@@ -0,0 +1,8 @@
+//
+//  Responsiveness.swift
+//  noki-demo
+//
+//  Created by rathan on 23/07/24.
+//
+
+import Foundation
